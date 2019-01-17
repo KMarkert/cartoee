57449329f5db29ccdba9ec40d88aa307d4e36fc7
kmarkert <kel.markert@gmail.com>
2019-01-17 01:06:06 -0600

    trying mock cython build

--- docs/conf.py
+++ docs/conf.py
@@ -174,5 +174,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['cython']
+MOCK_MODULES = ['cython','cartopy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
