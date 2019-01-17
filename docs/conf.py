3279402daaa0c996791cf4a033d717ff3f773293
kmarkert <kel.markert@gmail.com>
2019-01-17 00:46:09 -0600

    chages to docs conf paths

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 
 import os,sys,subprocess
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
